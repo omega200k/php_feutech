29fcef1940ac043d7634306a6c4004a946fd90b9
omegabomba2000 <coolguy.25@yahoo.com>
2026-04-30 13:20:20 +0800

    hello

---
+++ index.php
@@ -0,0 +1,124 @@
+<html>
+<title> RESUME </title>
+<head>
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Activities</title>
+    <style>
+        * {
+            font-family: Arial, Helvetica, sans-serif;
+
+        }
+body {
+    background-color: #6c74bdff;
+}
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+.card-info {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 50%;
+}
+.card-info:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+.contact-info{
+    padding-left: 20px;
+}
+.name{
+    font-size: 35px;
+}
+.container {
+  padding: 2px 16px;
+}
+.card-info {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 25px; 
+  background-color: #f1f1f1;
+}
+img {
+  border-radius: 25px 25px 0 0;
+}
+        
+    </style>
+</head>
+<body>
+<table>
+<tr>
+<div class="card-info">
+<img src="https://media.discordapp.net/attachments/590735643190100005/1499273824804274223/Reimone_Valdes_-_Headshot.JPG?ex=69f4330e&is=69f2e18e&hm=9068ec28a067d89f3f19ef47d30c1d4b909b446a34142ea08989834a571e8595&=&format=webp&width=350&height=350" width = "250" height ="250">
+<div class="contact-info">
+    <?php
+        $name = "Reimone Bradley T. Valdes";
+        $birthyear = 2006; 
+        $emailaddr = "coolguy.25@yahoo.com";
+        $address ="Nangka, Marikina";
+    ?>
+<b class="name"><?= "I am $name" ?> </b>
+<h2> <?= "Student@Feutech and Website Developer" ?> </h2> 
+<h3> <?= "Address: $address" ?> </h3>
+<h3> <?= "Email Address: $emailaddr" ?></h3>
+
+    </div>
+</div>
+ 
+</tr>
+<tr>
+<div class="card">
+<td><font size ="10"><b><u>PROFILE</u>	</b> </font></td><td></td><td><font size ="10"><b><u>CAREER</u></b></font><td></td><td></td></td>
+</tr>
+<tr> 
+<td>
+<h2> Welcome to the profile of Reimone Valdes, 
+<br>He is currently on the path of completing his education
+<br>on BSIT-CST and is working on a PHP based project. </h2>
+</td>
+<td>
+</td>
+<td>
+<h2>
+2024 - Present
+</h2>
+</td>
+<td>
+<h2> Photoeditor&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Adobe Photoshop</h2>
+<br> <h2> Participates in photoediting for profile pictures,
+<br>logos and editing or "photoshopping" photos that <br>
+need to be photoshopped.</h2>
+</td>
+</tr>
+<tr> 
+<td><h1><u>SKILLS</u></h1>
+<br> <h3> Knowledgable in Adobe Photoshop </h3> <h3> is currently learning to create programs
+using C++ and has some knowledge in html</td>
+<td>
+</td>
+<td> <h2>2018-2022</h2> </td>
+<td>
+<h2>Coder at freecodecamp&emsp;&emsp;&emsp;&emsp;freecodecamp.org </h2><br>
+<h2> He practiced coding at freecodecamp in order to enhance his <br>
+skills and build up knowledge about the languages he may encounter <br>
+during his education</h2>
+</tr>
+</div>
+<tr>
+<br>
+<td>
+<h1><u><b> EDUCATION </b></u></h1>
+<br> <h2> FEU Institute of Technology&emsp;&emsp;&emsp;Cybersecurity Technologies <br>
+2024-2028 </h2>
+<br> <h2> FEU Roosevelt Marikina&emsp;&emsp;&emsp;&emsp;Information Communications Technology <br>
+2022-2024 </h2>
+<br> <h2> San Lorenzo Ruiz De Manila School of Marikina&emsp;Information Communications Technology <br>
+2017-2022 </h2>
+</td>
+</table>
+</body>
+</html>
+
